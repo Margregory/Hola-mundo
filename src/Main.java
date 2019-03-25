0863fc48290997c96c9eccee282b99727cdbf00d
margs <margs@192.168.0.202>
2019-03-25 16:51:49 -0300

    primero

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+		int a;
+		a=10;
+		int b=2;
+		int c=0;
+		System.out.println("La variable a vale: "+a + " y la b vale: "+b);
+		c=a*b;
+		a=c;
+		System.out.println("Ahora la variable a vale: "+a + " y la b vale: "+b);
+		if (a<b) {
+			System.out.println("La variable a es mas pequeña que b");
+				} else {
+			System.out.println("La variable b es mas grande");
+
+		}
+	
+
+	}
+
+}
